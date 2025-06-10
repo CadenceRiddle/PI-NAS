c4104972261803df798f9ff9fc5be00a5a400057
CadenceRiddle <cadence475@gmail.com>
2025-06-09 21:39:54 -0400

    added method to check for arguments

--- pi_nas.c
+++ pi_nas.c
@@ -1,8 +1,17 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int main(void){
+int main(int argc, char *argv[]) {
 
+    if (argc != 2) { // Check if the correct number of arguments is provided
+        fprintf(stderr, "Usage: %s <path-to-local-file>\n", argv[0]);
+        return 1;
+    }
+
+    const char* local_file = argv[1]; // Local file path provided as command line argument
+    const char* remote_user = "USERNAME"; // Remote user
+    const char* remote_ip = "IP"; // VPN IP
+    const char* remote_path = "PATH";
     
 
     return 0;
