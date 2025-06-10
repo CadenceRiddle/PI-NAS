cc34feeaea5d90dab0be2f9ae7b8790dc0d85c8d
CadenceRiddle <cadence475@gmail.com>
2025-06-09 21:42:31 -0400

    added SCP instruction and execution

--- pi_nas.c
+++ pi_nas.c
@@ -13,6 +13,18 @@ int main(int argc, char *argv[]) {
     const char* remote_ip = "IP"; // VPN IP
     const char* remote_path = "PATH";
     
+    char command[512]; // Buffer to hold the SCP command
+    snprintf(command, sizeof(command), 
+        "scp \"%s\" %s@%s:%s",
+        local_file, remote_user, remote_ip, remote_path
+    ); // Construct the SCP command
+
+    int result = system(command); // Execute the SCP command
+
+    if (result != 0) {
+        fprintf(stderr, "Error: file transfer failed (scp exit code %d)\n", result);
+        return 1;
+    } // Check if the command was successful
 
     return 0;
 }
